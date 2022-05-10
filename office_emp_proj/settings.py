9d0a4ce3482de400757b9b591fc27db7b208c68a
Pawan <pawangupta5071@gmail.com>
2022-05-10 21:57:52 +0530

    cj

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-04n$_ed7imuc#a1)407lut0-rs%@i6#a(dbo--ihp2r4_lro$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
