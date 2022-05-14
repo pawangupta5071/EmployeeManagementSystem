76a1a5bf081e3ea13779022258120b3956eaa9e6
Pawan <pawangupta5071@gmail.com>
2022-05-14 12:20:08 +0530

    b

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-04n$_ed7imuc#a1)407lut0-rs%@i6#a(dbo--ihp2r4_lro$5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CSRF_COOKIE_SECURE=False
+CSRF_COOKIE_SECURE=True
+SESSION_COOKIE_SECURE=True
 
 CSRF_TRUSTED_ORIGINS=['https://*.empmanagesy.herokuapp.com']
 
